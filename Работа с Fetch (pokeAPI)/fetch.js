e9e88e7510edffc81638d50246545655710daab6
kopatich32 <kopatich1994@yandex.ru>
2023-10-01 12:54:22 +0300

    try to fix

--- Работа с Fetch (pokeAPI)/fetch.js
+++ Работа с Fetch (pokeAPI)/fetch.js
@@ -1,5 +1,5 @@
 let arr = [];
-let amountCard = 20;
+let amountCard = 26;
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
     while (0 !== currentIndex) {
@@ -11,41 +11,61 @@ function shuffle(array) {
     }
     return array;
 }
-
-for (let i = 1; i < amountCard + 1; i++) {
+for (let i = 1; i < amountCard+1; i++) {
     arr.push(i);
 }
-
 shuffle(arr);
+
 
 async function getPokemons(character) {
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${character}`);
     let data = await response.json();
     fillCards(data);
-
 }
+
 let currentAmount = 6;
 let more = document.querySelector('#more')
-
+let arrForDelete =[]
 function fetchRandom() {
-    for (let i = 0; i < currentAmount; i++) {
+    for (let i = 0; i < currentAmount ; i++) {
         let id = arr[i]
         getPokemons(id);
-        deleteUsed(id)
+        // deleteUsed(id)
+        arrForDelete.push(id)
+        // getPokemons(arrForDelete[i])
     }
 }
-fetchRandom();
+console.log(arr)
+let flag = false;
+if(!flag) {
+    fetchRandom();
+    lala()
+    flag = true;
+}
+
+function lala(){
+   for( let i = 0; i < arrForDelete.length; i++){
+       let res = arr.findIndex(item=>item == arrForDelete[i])
+        arr.splice(res,1)
+   }
+}
+console.log(arr)
+function fetchRandom2() {
+    for (let i = 0; i < currentAmount ; i++) {
+        let id = arr[i]
+        console.log(id)
+        getPokemons(id);
+        // arrForDelete.push(id)
+    }
+}
 
 function deleteUsed(usedNumber){
     let res = arr.findIndex(item=> item == usedNumber)
         arr.splice(res, 1)
-
 }
 
 let mainTag = document.querySelector('main');
-
 function fillCards(cardsData) {
-    for (let i = 0; i < 1; i++) {
         let card =
             `<div class="wrapper">
 <div class="card">
@@ -61,14 +81,32 @@ function fillCards(cardsData) {
 </div>
 </div>`;
         mainTag.insertAdjacentHTML('beforeend', card);
-    }
 }
 
 document.querySelector('#reload').onclick = () => location.reload()
 
 more.addEventListener('click', function () {
-    fetchRandom()
+    lala()
+    fetchRandom2()
+    console.log(arr)
         if(arr.length === 0){
             alert('Покемоны закончились!')
         }
 })
+
+
+
+// function contains(where, what) {
+//
+//     for (let i = 0; i < what.length; i++) {
+//         for (let j = 0; j < where.length; j++) {
+//             if (what[i] == where[i]) {
+// let res = where.find(item=> item == what[i])
+//                 console.log(res)
+//             }
+//         }
+//     }
+//     return 'est';
+// }
+//
+// console.log(contains(arr,arrForDelete))
